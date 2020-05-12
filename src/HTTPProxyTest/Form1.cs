9b5eed5c66586f48c83790f8c0462b276da05fff
Ryougi-G <1365654168@qq.com>
2020-05-13 00:05:46 +0800

    Create

---
+++ src/HTTPProxyTest/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace HTTPProxyTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int port = Convert.ToInt32(textBox2.Text);
+            string ip = null;
+            int rport = Convert.ToInt32(null);
+            arg a = new arg(ip, port, rport);
+            Thread th = new Thread(new ParameterizedThreadStart(ReadRequest));
+            th.Start(a);
+        }
+        private void ReadRequest(object ax)
+        {
+            arg a = (arg)ax;
+            TcpListener server = new TcpListener(IPAddress.Any,a.port);
+            server.Start();
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Thread nt = new Thread(DoProxy);
+                nt.Start(client);
+            }
+        }
+        private void DoProxy(object ox)
+        {
+            TcpClient client = (TcpClient)ox;
+            string content = "";
+            var ns = client.GetStream();
+            using (StreamReader reader = new StreamReader(ns))
+            {
+                while (ns.CanRead)
+                {
+                    string t = reader.ReadLine();
+                    if (String.IsNullOrEmpty(t))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        content += t;
+                        content += "\r\n";
+                    }
+                }
+                SetStrDelegate del = SetStr;
+                textBox1.Invoke(del, content);
+                try
+                {
+                    ResolvedHttpProxyRequest request = HTTPRequestParser.ResolveHTTPProxyRequest(content);
+                    TcpClient cst = new TcpClient();
+                    string[] spchost = request.Host.Split(':');
+                    // MessageBox.Show(spchost[0]);
+                    //MessageBox.Show(spchost[1]);
+                    string s = HTTPRequestParser.DeleteFrontWhiteSpace(spchost[0]);
+                    cst.Connect(Dns.GetHostEntry(HTTPRequestParser.DeleteFrontWhiteSpace(spchost[0])).AddressList[0], Convert.ToInt32(spchost[1]));
+                    textBox1.Invoke(del, request.ResolvedRequest);
+                    NetworkStream serverstream = cst.GetStream();
+                    serverstream.Write(Encoding.ASCII.GetBytes(request.ResolvedRequest), 0, Encoding.ASCII.GetByteCount(request.ResolvedRequest));
+                    BridgeCon(serverstream, ns);
+                    File.WriteAllText("test.txt", content);
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+        }
+        private delegate void SetStrDelegate(string mess);
+        private void SetStr(string mess)
+        {
+            textBox1.Text = mess;
+        }
+        private void BridgeCon(NetworkStream sns,NetworkStream cns)
+        {
+            using(BinaryReader reader=new BinaryReader(sns))
+            {
+                using(BinaryWriter writer=new BinaryWriter(cns))
+                {
+                    while (sns.CanRead && cns.CanWrite)
+                    {
+                        try
+                        {
+                            byte[] buffer = new byte[1024];
+                            int hasRead = reader.Read(buffer, 0, 1024);
+                            writer.Write(buffer, 0, hasRead);
+                        }catch(Exception ex)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    class arg
+    {
+        public string ip;
+        public int port;
+        public int rport;
+        public arg(string i,int p,int r)
+        {
+            ip = i;
+            port = p;
+            rport=r;
+        }
+    }
+}
---
+++ src/HTTPProxyTest/HTTPRequestParse.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTTPProxyTest
+{
+    static class HTTPRequestParser
+    {
+        public static string DeleteFrontWhiteSpace(string str)
+        {
+            int pos = 0;
+            while (str[pos] == ' ' && pos < str.Length)
+            {
+                pos++;
+            }
+            if (pos < str.Length)
+            {
+                string res = "";
+                for (int i = pos; i < str.Length; i++)
+                {
+                    res += str[i];
+                }
+                return res;
+            }
+            else
+            {
+                return str;
+            }
+        }
+        public static ResolvedHttpProxyRequest ResolveHTTPProxyRequest(string rawRequest)
+        {
+            char[] spc= { '\r','\n'};
+            string[] allHeaders = rawRequest.Split(spc);
+            string[] firstLineParas = allHeaders[0].Split(' ');
+            string Method = firstLineParas[0];string URI = firstLineParas[1];string HttpInfo = firstLineParas[2];
+            List<HeaderPair> rawHeader=new List<HeaderPair>();
+            List<string> Connection=new List<string>();
+            List<string> ProxyConnection = new List<string>();
+            string Host="";
+            for(int i=1;i<allHeaders.Length;i++)
+            {
+                if (String.IsNullOrWhiteSpace(allHeaders[i]))
+                {
+                    continue;
+                }
+                string header = allHeaders[i];
+                string[] head=header.Split(':');
+                string key = head[0];
+                string value = DeleteFrontWhiteSpace(head[1]);
+                rawHeader.Add(new HeaderPair(key, value));
+                if (key.ToLower() == "connection")
+                {
+                    foreach(string s in value.Split(','))
+                    {
+                        Connection.Add(s);
+                    }
+                }
+                if (key.ToLower() == "proxy-connection")
+                {
+                    foreach (string s in value.Split(','))
+                    {
+                        ProxyConnection.Add(s);
+                    }
+                }
+                if (key.ToLower() == "host")
+                {
+                    Host = HTTPRequestParser.DeleteFrontWhiteSpace(head[1] + ":" + head[2]);
+                }
+            }
+            List<HeaderPair> resovledheaders = new List<HeaderPair>();
+            foreach(HeaderPair raw in rawHeader)
+            {
+                if(raw.Key.ToLower() == "proxy-connection")
+                {
+                    continue;
+                }
+                resovledheaders.Add(raw);
+            }
+            foreach(HeaderPair header in resovledheaders)
+            {
+                if (header.Key.ToLower() == "connection")
+                {
+                    header.Value = "close";
+                }
+                if (header.Key.ToLower() == "proxy-connection")
+                {
+                    header.Value = "close";
+                }
+            }
+            string resolvedreq = "";
+            resolvedreq += Method+" ";
+            resolvedreq += URI+" ";
+            resolvedreq += HttpInfo+"\r\n";
+            foreach(HeaderPair header in resovledheaders)
+            {
+                resolvedreq += header.ToString() + "\r\n";
+            }
+            resolvedreq += "\r\n";
+            ResolvedHttpProxyRequest result = new ResolvedHttpProxyRequest(Host, Method, Connection, ProxyConnection, rawRequest, resolvedreq, URI, HttpInfo, rawHeader, resovledheaders);
+            return result;
+        }
+    }
+    public class ResolvedHttpProxyRequest
+    {
+        public string Host
+        {
+            get;
+            private set;
+        }
+        public string Method
+        {
+            get;
+            private set;
+        }
+        public List<string> Connection
+        {
+            get;
+            private set;
+        }
+        public List<string> Proxy_Connection
+        {
+            get;
+            private set;
+        }
+        public string RawRequest
+        {
+            get;
+            private set;
+        }
+        public string ResolvedRequest
+        {
+            get;
+            private set;
+        }
+        public string URI
+        {
+            get;
+            private set;
+        }
+        public string HTTPInfo
+        {
+            get;
+            private set;
+        }
+        public List<HeaderPair> RawHeadersExceptFirstLine
+        {
+            get;
+            private set;
+        }
+        public List<HeaderPair> ResolvedHeadersExceptFirstLine
+        {
+            get;
+            set;
+        }
+        public ResolvedHttpProxyRequest(string host,string method,List<string> connection,List<string> proxy_connection,string rawRequest,string resolvedrequest,string uri,string httpver,List<HeaderPair> rawheaders,List<HeaderPair> resheaders)
+        {
+            Host = host;
+            Method = method;
+            Connection = connection;
+            Proxy_Connection = proxy_connection;
+            RawRequest = rawRequest;
+            ResolvedRequest = resolvedrequest;
+            URI = uri;
+            HTTPInfo = httpver;
+            RawHeadersExceptFirstLine = rawheaders;
+            ResolvedHeadersExceptFirstLine = resheaders;
+        }
+        
+    }
+    public class HeaderPair
+    {
+        public string Key
+        {
+            get;
+            private set;
+        }
+        public  string Value
+        {
+            get;
+            set;
+        }
+        public HeaderPair(string key,string value)
+        {
+            Key = key;
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return Key + ":" + Value;
+        }
+    }
+}
